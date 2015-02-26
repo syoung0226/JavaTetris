7468a5228dd53a89efa883076a881998537b7d5d
sunyoung <syoung0226@gmail.com>
2015-02-26 11:26:07 +0900

    JBlock and LBlock can change shape

--- src/block/JBlock.java
+++ src/block/JBlock.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 
 public class JBlock extends Block{
 	
+	private int step = 0;
+	
 	public JBlock() {
 		super();
 		initialize();
@@ -13,12 +15,41 @@ public class JBlock extends Block{
 	public void initialize() {
 		super.setInitializeCell(Color.cyan, 6, 0, 0);
 		for(int i=1; i< super.getCellsSize(); i++){
-			super.setInitializeCell(Color.cyan, 3+i, 1, i);
+			super.setInitializeCell(Color.cyan, 7-i, 1, i);
 		}
 	}
 
 	@Override
 	public void changeShape() {
+		switch (step){
+		case 0:
+			super.setCell(super.getCell(0).getX()-1, super.getCell(0).getY()+1, 0);
+			super.setCell(super.getCell(1).getX()-2, super.getCell(1).getY(), 1);
+			super.setCell(super.getCell(2).getX()-1, super.getCell(2).getY()-1, 2);
+			super.setCell(super.getCell(3).getX(), super.getCell(3).getY()-2, 3);
+			step = 1;
+			break;
+		case 1:
+			super.setCell(super.getCell(0).getX()-1, super.getCell(0).getY(), 0);
+			super.setCell(super.getCell(1).getX(), super.getCell(1).getY()-1, 1);
+			super.setCell(super.getCell(2).getX()+1, super.getCell(2).getY(), 2);
+			super.setCell(super.getCell(3).getX()+2, super.getCell(3).getY()+1, 3);
+			step = 2;
+			break;
+		case 2:
+			super.setCell(super.getCell(0).getX()+1, super.getCell(0).getY()-2, 0);
+			super.setCell(super.getCell(1).getX()+2, super.getCell(1).getY()-1, 1);
+			super.setCell(super.getCell(2).getX()+1, super.getCell(2).getY(), 2);
+			super.setCell(super.getCell(3).getX(), super.getCell(3).getY()+1, 3);
+			step = 3;
+			break;
+		case 3:
+			super.setCell(super.getCell(0).getX()+1, super.getCell(0).getY()+1, 0);
+			super.setCell(super.getCell(1).getX(), super.getCell(1).getY()+2, 1);
+			super.setCell(super.getCell(2).getX()-1, super.getCell(2).getY()+1, 2);
+			super.setCell(super.getCell(3).getX()-2, super.getCell(3).getY(), 3);
+			step = 0;
+			break;
+		}
 	}
-
 }
--- src/block/LBlock.java
+++ src/block/LBlock.java
@@ -30,12 +30,25 @@ public class LBlock extends Block{
 			step = 1;
 			break;
 		case 1:
+			super.setCell(super.getCell(0).getX()+1, super.getCell(0).getY()+2, 0);
+			super.setCell(super.getCell(1).getX()+2, super.getCell(1).getY()+1, 1);
+			super.setCell(super.getCell(2).getX()+1, super.getCell(2).getY(), 2);
+			super.setCell(super.getCell(3).getX(), super.getCell(3).getY()-1, 3);
+			step = 2;
 			break;
 		case 2:
+			super.setCell(super.getCell(0).getX()-1, super.getCell(0).getY(), 0);
+			super.setCell(super.getCell(1).getX(), super.getCell(1).getY()+1, 1);
+			super.setCell(super.getCell(2).getX()+1, super.getCell(2).getY(), 2);
+			super.setCell(super.getCell(3).getX()+2, super.getCell(3).getY()-1, 3);
+			step = 3;
 			break;
 		case 3:
-			break;
-		case 4:
+			super.setCell(super.getCell(0).getX()-1, super.getCell(0).getY()-1, 0);
+			super.setCell(super.getCell(1).getX()-2, super.getCell(1).getY(), 1);
+			super.setCell(super.getCell(2).getX()-1, super.getCell(2).getY()+1, 2);
+			super.setCell(super.getCell(3).getX(), super.getCell(3).getY()+2, 3);
+			step = 0;
 			break;
 		}
 	}
