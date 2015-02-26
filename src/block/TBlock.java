0b267ac8ee1c8e8bd382767806bdb256a21abb5f
sunyoung <syoung0226@gmail.com>
2015-02-26 11:34:46 +0900

    TBlock can change shape

--- src/block/TBlock.java
+++ src/block/TBlock.java
@@ -3,6 +3,8 @@ package block;
 import java.awt.Color;
 
 public class TBlock extends Block{
+	
+	private int step = 0;
 
 	public TBlock() {
 		super();
@@ -17,7 +19,34 @@ public class TBlock extends Block{
 			super.setInitializeCell(Color.gray, 4+i, 1, 1+i);
 		}
 	}
+
 	@Override
 	public void changeShape() {
+		switch (step){
+		case 0:
+			super.setCell(super.getCell(1).getX(), super.getCell(1).getY()-2, 1);
+			super.setCell(super.getCell(2).getX()-1, super.getCell(2).getY()-1, 2);
+			super.setCell(super.getCell(3).getX()-2, super.getCell(3).getY(), 3);
+			step = 1;
+			break;
+		case 1:
+			super.setCell(super.getCell(1).getX()+2, super.getCell(1).getY(), 1);
+			super.setCell(super.getCell(2).getX()+1, super.getCell(2).getY()-1, 2);
+			super.setCell(super.getCell(3).getX(), super.getCell(3).getY()-2, 3);
+			step = 2;
+			break;
+		case 2:
+			super.setCell(super.getCell(1).getX(), super.getCell(1).getY()+2, 1);
+			super.setCell(super.getCell(2).getX()+1, super.getCell(2).getY()+1, 2);
+			super.setCell(super.getCell(3).getX()+2, super.getCell(3).getY(), 3);
+			step = 3;
+			break;
+		case 3:
+			super.setCell(super.getCell(1).getX()-2, super.getCell(1).getY(), 1);
+			super.setCell(super.getCell(2).getX()-1, super.getCell(2).getY()+1, 2);
+			super.setCell(super.getCell(3).getX(), super.getCell(3).getY()+2, 3);
+			step = 0;
+			break;
+		}
 	}
 }
